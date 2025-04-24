6421d56c038fd60fa838eea4f24bb66f5a04066b
Carlos <carlos97181034535@gmail.com>
2025-04-24 15:27:09 +0200

    added: 2 html elements and an object

--- src/js/game/constants.js
+++ src/js/game/constants.js
@@ -4,3 +4,7 @@ export const $tries = document.querySelector('#tries');
 export const $score = document.querySelector('#score');
 export const $combo = document.querySelector('#combo');
 export const $question = document.querySelector('#question');
+export const $answer = document.querySelector('#answer');
+export const $form = document.querySelector('#form');
+
+export const userAnswerObj = { userAnswer: '' };
