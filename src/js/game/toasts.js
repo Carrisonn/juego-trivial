487a3f0f881b5be48d2563ff33bced2fa67f03b5
Carrison <carlos97181034535@gmail.com>
2025-04-27 03:08:40 +0200

    added: toasts to provide feedback

---
+++ src/js/game/toasts.js
@@ -0,0 +1,33 @@
+import Swal from 'sweetalert2'
+
+export const bonusToast = Swal.mixin({
+  toast: true,
+  position: "top",
+  showConfirmButton: false,
+  timer: 7000,
+  theme: 'dark',
+  background: '#582c8a',
+  padding: '0.5rem',
+  color: '#fff'
+});
+
+export const correctAnswerToast = Swal.mixin({
+  toast: true,
+  position: "top",
+  showConfirmButton: false,
+  timer: 4000,
+  theme: 'dark',
+  background: '#5bbd63',
+  padding: '0.5rem',
+  color: '#fff'
+});
+
+export const errorToast = Swal.mixin({
+  toast: true,
+  position: "top",
+  showConfirmButton: false,
+  timer: 4000,
+  background: '#da3a3a',
+  padding: '0.5rem',
+  color: '#fff'
+});
