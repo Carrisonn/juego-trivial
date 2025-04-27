67692695861e1b3ff5c5dd3cc7566fbb39498207
Carrison <carlos97181034535@gmail.com>
2025-04-27 03:07:53 +0200

    added: handle when user guess the answer

--- src/js/game/main.js
+++ src/js/game/main.js
@@ -1,14 +1,14 @@
-import Swal from 'sweetalert2'
 import { topicsSet } from './topicSets.js';
 import { $titleTopic, $lives, $tries, $score, $combo, $question, $answer, $form, userAnswerObj } from './constants.js';
+import { bonusToast, correctAnswerToast, errorToast } from './toasts.js';
 
 
 // States
-const conditionBonus = 5;
 let lives = 5;
 let tries = 3;
 let score = 0;
-let combo = 0; // if user answered correctly 5 times in a row the user receives a bonus (+1 live and +50 score)
+let combo = 0; // increases when the user guesses the answers in a row
+let bonusCondition = 5;  // combo === bonus condition => reward
 let questionPosition = 0; // topic object => questionsSet is array of objects => 0 is first object(contains question and answer)
 
 
@@ -21,11 +21,10 @@ function handleTopicSelected() {
 
   const topicSelectedByUser = topicsSet[topicPosition]; // injecting the postion of the topic set selected in the array of topics
   const { questionsSet } = topicSelectedByUser
+
   handleTitleTopic(topicSelectedByUser);
   renderTopicSelected(questionsSet);
-
-  //$answer.addEventListener('input', handleUserAnswer)
-  $form.addEventListener('submit', event => validateAnswer(event, questionsSet));
+  handleEventListeners(questionsSet)
 }
 
 function handleTitleTopic(topicSet) {
@@ -43,45 +42,65 @@ function renderTopicSelected(questionsSet) {
   $combo.textContent = combo;
   $question.textContent = question;
 
-  $answer.value = '';
+  userAnswerObj.userAnswer = '';
   $form.reset();
+}
+
+function handleEventListeners(questionsSet) {
+  $answer.addEventListener('input', handleUserAnswer)
+  $form.addEventListener('submit', event => validateAnswer(event, questionsSet));
+}
+
+function handleUserAnswer(event) {
+  userAnswerObj.userAnswer = event.target.value.trim().toLowerCase();
 }
 
 function validateAnswer(event, questionsSet) {
   event.preventDefault();
 
-  const userAnswer = $answer.value.toLowerCase().trim();
   const correctAnswer = questionsSet[questionPosition].answer.toLowerCase().trim();
+  const { userAnswer } = userAnswerObj
+  const emptyValue = userAnswer === '';
   const isCorrectAnswer = userAnswer === correctAnswer;
-  const isIncorrectAnswer = userAnswer !== correctAnswer;
+  //const isIncorrectAnswer = userAnswer !== correctAnswer;
 
+  if (emptyValue) return errorToast.fire({ title: 'Debes introducir una respuesta' });
   if (isCorrectAnswer) return handleCorrectAnswer(questionsSet);
-  if (isIncorrectAnswer) return handleIncorrectAnswer(questionsSet);
+  //if (isIncorrectAnswer) return handleIncorrectAnswer(questionsSet);
 }
 
 function handleCorrectAnswer(questionsSet) {
+  correctAnswerToast.fire({ title: '+10 Puntos' });
   score += 10;
   combo += 1;
   tries = 3;
 
-  if (combo === conditionBonus) return handleConditionBonus(questionsSet);
-  if (questionPosition + 1 === questionsSet.length) return handleFinishGame();
-
-  questionPosition += 1;
-  renderTopicSelected(questionsSet);
-}
-
-function handleConditionBonus(questionsSet) {
-  Swal.fire({ title: "!Felicidades!", text: "Has conseguido un bonus de 1 vida y 50 puntos" });
-  lives++;
-  score += 50;
-  combo = 0;
-
+  if (combo === bonusCondition) return handleUserReward(questionsSet);
   if (questionPosition + 1 === questionsSet.length) return handleFinishGame();
 
   questionPosition++;
   renderTopicSelected(questionsSet);
 }
+
+//function handleIncorrectAnswer(questionsSet) {
+//  console.log('INCORRECT ANSWER');
+//}
+
+function handleUserReward(questionsSet) {
+  bonusToast.fire({ title: `Has conseguido encadenar ${combo} respuestas seguidas, recibes 1 vida y 50 puntos` });
+  lives++;
+  score += 50;
+
+  if (questionPosition + 1 === questionsSet.length) return handleFinishGame();
+  combo = 0;
+  bonusCondition += 2;
+  questionPosition++;
+  renderTopicSelected(questionsSet);
+}
+
+//function handleFinishGame() {
+//  console.log('FINISH GAME');
+//}
 
 //Game Init
 window.addEventListener('load', () => $form.reset());
