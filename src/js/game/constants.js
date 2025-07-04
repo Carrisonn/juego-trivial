ebcac16932ca11e791831da79c1f078c36dd887a
Carrison <carlos97181034535@gmail.com>
2025-07-04 21:54:51 +0200

    added: html button

--- src/js/game/constants.js
+++ src/js/game/constants.js
@@ -8,6 +8,7 @@ export const $answer = document.querySelector('#answer')
 export const $form = document.querySelector('#form')
 export const $containerGame = document.querySelector('.container-game')
 export const $btnSkipQuestion = document.querySelector('.btn-skip-question')
-export const $btnBackToMenu = document.querySelector('#btn-back-to-menu')
+export const $btnBackToMenu = document.querySelector('.btn-back-to-menu')
+export const $btnGetLives = document.querySelector('.btn-get-lives')
 
 export const userAnswerObj = { userAnswer: '' }
